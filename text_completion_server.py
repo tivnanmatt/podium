a416a302428580bc8b276854e3cd22a9d6bb18df
tivnanmatt <tivnanmatt@gmail.com>
2022-12-19 01:14:40 -0500

    made changes to the python server and html

--- text_completion_server.py
+++ text_completion_server.py
@@ -11,39 +11,50 @@ api_key = fileObject.read()
 api_key = api_key.replace("\n","")
 openai.api_key = api_key
 
-def generate_response(prompt):
+
+def generate_response(userInput,temperature=None):
   # Use the GPT-3 API to generate a response to the prompt
   response = openai.Completion.create(
     engine="text-davinci-003",
-    prompt=prompt,
-    max_tokens=2048,
-    temperature=0.7,
+    prompt=userInput,
+    max_tokens=4096-len(userInput),
+    temperature=temperature,
   )
-
   # Return the generated response
   return response['choices'][0].text
+
+
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        # Send response
-        self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+  def do_POST(self):
+    # Send response
+    self.send_response(200)
+    self.send_header("Access-Control-Allow-Origin", "*")
+    self.end_headers()
 
-        # Get request data
-        content_length = int(self.headers["Content-Length"])
-        body = self.rfile.read(content_length)
+    # Get request data
+    content_length = int(self.headers["Content-Length"])
+    body = self.rfile.read(content_length)
 
-        # Parse the request data as JSON
-        request_json = json.loads(body)
+    # Parse the request data as JSON
+    request_json = json.loads(body)
 
-        # Extract the userInput value
-        user_input = request_json["userInput"]
+    # Set the temperature for AI text generation, if provided
+    if request_json.get("temperature", None) is not None:
+      temperature = request_json.get("temperature", None)
+    else:
+      temperature = 0.7
+      
+    # if textCompletion is provided, use it as the prompt
+    if request_json.get("textCompletion", None) is not None:
+      prompt = request_json.get("textCompletion", None)
+    
+      # Generate a response using the generate_response function
+      response = generate_response(prompt,self.temperature)
 
-        # Generate a response using the generate_response function
-        response = generate_response(prompt=user_input)
-
-        # Send the response to the client
-        self.wfile.write(response.encode())
+      # Send the response to the client
+      self.wfile.write(response.encode())
 
 # Start the server
 httpd = HTTPServer(("0.0.0.0", 8000), RequestHandler)
